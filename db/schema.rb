cd2e35935e023dba0be17ce9fdb573c88ec34d04
yukiti224gt <mamekiti084@gmail.com>
2021-10-13 06:11:20 +0000

    [ADD] name追加　

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_10_13_012450) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
