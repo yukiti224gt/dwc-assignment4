a623d032e65fb3aecb809bf652277ef411dd7d37
yukiti224gt <mamekiti084@gmail.com>
2021-10-14 09:17:38 +0000

    [ADD] 投稿機能と表示まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,11 +3,15 @@ class BooksController < ApplicationController
   end
 
   def create
-    
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
   end
 
   def index
     @book = Book.new
+    @books = Book.all
   end
 
   def show
