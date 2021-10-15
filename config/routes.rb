4acf525c331eafd743f23454314d70f56f32544a
yukiti224gt <mamekiti084@gmail.com>
2021-10-15 06:42:26 +0000

    [ADD] 投稿削除，alert出力

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#top'
   
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index]
   resources :books, only: [:new, :create, :index, :show, :destroy]
 end
