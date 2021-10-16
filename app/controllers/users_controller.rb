a2bf058b11c8335b3407551fbd0706a43c600c41
yukiti224gt <mamekiti084@gmail.com>
2021-10-16 06:56:35 +0000

    [ADD] 更新まで（ファイルは未表示（

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,16 @@ class UsersController < ApplicationController
   end
   
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
   
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
