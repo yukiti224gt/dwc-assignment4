c1b017fc9a56d1a58ed39f1a3d6962372b338c67
yukiti224gt <mamekiti084@gmail.com>
2021-10-16 15:32:28 +0000

    [ADD] 全体的（about)の画面遷移はまだ

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'home#top'
-  
+  get  '/home/about/', to: 'home#index' 
   resources :users, only: [:show, :index, :edit, :update]
   resources :books, only: [:new, :create, :index, :show, :destroy]
 end
