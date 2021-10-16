5a3d9b7c72579e1c6523359bb5a8601027811726
yukiti224gt <mamekiti084@gmail.com>
2021-10-16 07:54:43 +0000

    [ADD] ファイル表示

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '4795e621a8a0ee57c00e437fecb2eab2ea77f15c192cfa1d46a00358ee897fefa8d98f12644ee2b6e631cc2735fbfc78b310808ef5f68c8af63855cf4e3d20f3'
