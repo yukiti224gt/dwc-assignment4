f27c2623b989d52a455d5f687ec7745fcf26e301
yukiti224gt <mamekiti084@gmail.com>
2021-10-16 09:35:37 +0000

    [ADD] バリデーションと設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
   
   has_many :books, dependent: :destroy
   attachment :profile_image
+  
+  validates :name, presence: true, uniqueness: true, length: { minimum:2, maximum:20 }
+  validates :introduction, length: { maximum: 50 }
 end
