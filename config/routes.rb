990a686fe33bb0f2ea153070bac6a7e024ec6357
yukiti224gt <mamekiti084@gmail.com>
2021-10-19 16:31:47 +0000

    [ADD] routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   root to: 'home#top'
   get  '/home/about/', to: 'home#index' 
   resources :users, only: [:show, :index, :edit, :update, :create]
-  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
+    resources :book_comments, only: [:create, :destroy]
+  end
 end
