75f45d35703bcefbd32dcb9106c4c9b825d7937d
yukiti224gt <mamekiti084@gmail.com>
2021-10-20 00:36:35 +0000

    [ADD] book_commentコントローラー

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,9 +1,21 @@
 class BookCommentsController < ApplicationController
   def create
-  
+    book = Book.find(params[:book_id])
+    book_comment = current_user.book_comments.new(book_comment_params)
+    book_comment.book_id = book.id
+    if book_comment.save
+      flash[:notice] = "save was done"
+      redirect_to books_path
+    else
+      redirect_to book_path(book.id)
+    end
   end
   
   def destroy
   
+  end
+
+  def book_comment_params
+      params.require(:book_comment).permit(:comment)
   end
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,6 +26,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @book_new = Book.new
+    @book_comment = BookComment.new
   end
 
   def destroy
@@ -55,4 +56,5 @@ class BooksController < ApplicationController
     def book_params
       params.require(:book).permit(:title, :body)
     end
+    
 end
