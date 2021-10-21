dfd14c4641f8ed6b37e6beeca6c8cfde5dc7397b
yukiti224gt <mamekiti084@gmail.com>
2021-10-21 11:00:54 +0000

    [ADD] Favorite の効果

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,15 @@
 class FavoritesController < ApplicationController
-  belongs_to :user
-  belongs_to :book
+  def create
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: book.id)
+    favorite.save
+    redirect_to book_path(book)
+  end
+  
+  def destroy
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: book.id)
+    favorite.destroy
+    redirect_to book_path(book)
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -5,4 +5,8 @@ class Book < ApplicationRecord
   
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
+
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 end
