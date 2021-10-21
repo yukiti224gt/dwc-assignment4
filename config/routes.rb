59b31c66f4482ab8e4570fc4e2e2a32019cf1c83
yukiti224gt <mamekiti084@gmail.com>
2021-10-21 08:17:51 +0000

    [ADD] Favorite routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   root to: 'home#top'
   get  '/home/about/', to: 'home#index' 
   resources :users, only: [:show, :index, :edit, :update, :create]
+  
   resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
+    resources :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
 end
