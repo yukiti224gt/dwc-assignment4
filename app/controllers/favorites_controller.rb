7b456e27d706358f8508f68bbb856b09cebf26de
yukiti224gt <mamekiti084@gmail.com>
2021-10-21 12:16:58 +0000

    [FIX] 細かい修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,7 +3,11 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    redirect_to book_path(book)
+    if book.user == current_user
+      redirect_to user_path(book.user)
+    else
+      redirect_to book_path(book)
+    end
   end
   
   def destroy
