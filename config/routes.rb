e0f67fc62ced0e2c4e42423f6ed13057a5819ffb
yukiti224gt <mamekiti084@gmail.com>
2021-10-22 01:28:08 +0000

    [FIX] routesの修正とpathの修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :index, :edit, :update, :create]
   
   resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
-    resources :favorites, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
 end
