e8977c36896c75254f31c932c13d59e5c6b29611
yukiti224gt <mamekiti084@gmail.com>
2021-10-24 06:12:15 +0000

    [ADD] いいねバリデーション他のやり方

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,5 +2,6 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :book
   
-  validates_uniqueness_of :book_id, scope: :user_id
+  # validates_uniqueness_of :book_id, scope: :user_id
+  validates :book_id, uniqueness: { scope: :user_id }
 end
